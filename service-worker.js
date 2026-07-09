b19b04a30872139ae505bc7c09b140786e4d48b3
loictiomo9-art <loictiomo9@gmail.com>
2026-07-09 15:47:51 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'chess-1500-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js',
+    '/js/chess-engine.js',
+    '/js/ai.js',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
+        })
+    );
+});
